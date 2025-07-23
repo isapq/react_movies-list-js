08e6e8b7fb130211af58589e4147f56326219bd2
Isaque Pereira dos Santos <isaquevidaltube@gmail.com>
2025-07-23 20:28:04 -0300

    add task solution

--- src/components/MovieCard/MovieCard.jsx
+++ src/components/MovieCard/MovieCard.jsx
@@ -1,3 +1,35 @@
 import './MovieCard.scss';
 
-export const MovieCard = () => <>Put the card here</>;
+export const MovieCard = ({ item }) => (
+  <div className="card" data-cy="Movie">
+    <div className="card-image">
+      <figure className="image is-4by3">
+        <img data-cy="MovieImage" src={item.imgUrl} alt="Film logo" />
+      </figure>
+    </div>
+
+    <div className="card-content">
+      <div className="media">
+        <div className="media-left">
+          <figure className="image is-48x48">
+            <img src={item.imgUrl} alt="imdb" />
+          </figure>
+        </div>
+
+        <div className="media-content">
+          <p className="title is-5" data-cy="MovieTitle">
+            {item.title}
+          </p>
+        </div>
+      </div>
+
+      <div className="content">
+        <p data-cy="MovieDescription">{item.description}</p>
+
+        <a href={item.imdbUrl} data-cy="MovieLink">
+          IMDB
+        </a>
+      </div>
+    </div>
+  </div>
+);
--- src/components/MovieList/MovieList.jsx
+++ src/components/MovieList/MovieList.jsx
@@ -1,3 +1,13 @@
+import React from 'react';
 import './MovieList.scss';
+import { MovieCard } from '../MovieCard/MovieCard';
 
-export const MovieList = () => <>Put the list here</>;
+export const MovieList = ({ list }) => {
+  return (
+    <div className="movies" data-cy="movieList">
+      {list.map(movie => (
+        <MovieCard key={movie.imdbId} item={movie} />
+      ))}
+    </div>
+  );
+};
